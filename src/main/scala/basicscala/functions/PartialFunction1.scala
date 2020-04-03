7dce96eab5f263ce32ddde6ddcf2fe8c4a9bc846
afz <dropmeataamir@gmail.com>
2020-04-03 14:22:40 +0530

    lift pf

--- src/main/scala/basicscala/functions/PartialFunction1.scala
+++ src/main/scala/basicscala/functions/PartialFunction1.scala
@@ -10,7 +10,14 @@ object PartialFunction1 extends App {
   val pf: PartialFunction[Int, Boolean] = { case i if i > 0 => i % 2 == 0}
   val liftedPF: Int => Option[Boolean] = pf.lift
 
-  val l: PartialFunction[Int, Int] = List(1, 2, 3)
+  def l[A]:PartialFunction[Int, Int] = List[Int](1, 2, 3)
   val lifted: Int => Option[Int] = l.lift
   val x: Option[Int] = lifted(0)
+
+  /**
+   * val l:PartialFunction[Int, A] = List ...Int index, A type of value
+   * val m:Int => Option[A] = l.lifted
+   */
+
+
 }
