72a08f82b2f47f0293cd55189cb32dc9706c2c65
Aamir Wani <aamir.wani@BRILLIO.COM>
2020-05-22 01:29:42 +0530

    call by name examples

--- src/main/scala/advancedscala/CallByName.scala
+++ src/main/scala/advancedscala/CallByName.scala
@@ -1,10 +1,34 @@
 package advancedscala
 
+import scala.concurrent.Future
+import scala.util.Try
+
 object CallByName1 extends App {
 
-   def byValueFunction(x: Int) = 34
-    byNameFunction(2+3)
+  def byValueFunction(x: Int) = 34
 
-   def byNameFunction(x: => Int) = 90
-    byNameFunction(2+3)
+  byNameFunction(2 + 3)
+
+  def byNameFunction(x: => Int) = 90
+
+  byNameFunction(2 + 3)
+
+  abstract class MyList[+A] {
+    def head: A
+
+    def tail: MyList[A]
+  }
+
+  class NonEmptyList[+A](h: => A, t: => MyList[A]) extends MyList[A] {
+    lazy val head: A = h
+    lazy val tail: MyList[A] = t
+  }
+
+  // hold the door
+  val anAttempt = Try(throw new NullPointerException
+    import scala.concurrent.ExecutionContext.Implicits.global
+  val f = Future {
+    //hard computation
+  }
+
 }
--- target/scala-2.12/classes/promisesfutures/ws.sc
+++ target/scala-2.12/classes/promisesfutures/ws.sc
@@ -4,14 +4,7 @@ val f = Future{
   ""
 }
 val p = Promise[Int]
-val f = p.future
+//val f = p.future
 
 p success(10)
-
-case class TaxCut(reduction: Int)
-val taxcut = Promise[TaxCut]()
-val taxcut2: Promise[TaxCut] = Promise()
-val taxcutF: Future[TaxCut] = taxcut.future
-
-taxcut.success(TaxCut(20))
 
