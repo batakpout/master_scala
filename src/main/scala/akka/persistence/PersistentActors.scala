21d67f09bdd350fbdbbd8a86d20e1b87f329056d
Aamir Wani <aamir.wani@BRILLIO.COM>
2020-05-29 01:05:29 +0530

    mnoved persistence to separate repo

---
+++ src/main/scala/akka/persistence/PersistentActors.scala
@@ -0,0 +1,9 @@
+package akka.persistence
+
+object PersistentActors extends App {
+  /**
+   * Scenario:we have a business and an accountant which keeps track of our invoices.
+   */
+  //class Accountant extends PersistentActors
+  //moved to separate repo
+}
