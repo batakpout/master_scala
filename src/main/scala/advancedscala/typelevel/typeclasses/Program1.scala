8f4f787d12d734e8ef0559845bc9d684c4a00941
ssspl <ssspl@ssspls-MacBook-Pro.local>
2020-08-22 13:39:58 +0530

    type classes

---
+++ src/main/scala/advancedscala/typelevel/typeclasses/Program1.scala
@@ -0,0 +1,78 @@
+package advancedscala.typelevel.typeclasses
+
+case class Cat(name: String, age: Int, color: String)
+
+//The Type class
+trait Printable[A] {
+  def format(value: A): String
+}
+
+//The Type Instance
+object PrintableInstance {
+
+  implicit val stringInstance = new Printable[String] {
+    override def format(value: String): String = value.take(4)
+  }
+
+  implicit val intInstance = new Printable[Int] {
+    override def format(value: Int): String = value.toString.take(2)
+  }
+
+  implicit val catInstance = new Printable[Cat] {
+    override def format(value: Cat): String =
+      s"${value.color} ${value.name} is ${value.age} years old"
+  }
+
+}
+
+//type interface
+object Printable {
+  def format[A](value: A)(implicit print: Printable[A]): String = print.format(value)
+  def print[A](value: A)(implicit print: Printable[A]) = println(print.format(value))
+}
+
+//type interface
+object PrintableSyntax {
+  implicit class PrintOps[A](value: A) {
+     def print(implicit p: Printable[A]) = p.format(value)
+  }
+}
+
+object Program1Test extends App {
+  import Printable._
+  import PrintableInstance._
+  println(format(100).trim())
+  print("yella")
+
+}
+
+object Program2Test extends App {
+  import PrintableSyntax._
+  import PrintableInstance._
+  Cat("pussy", 12, "white").print
+}
+
+object jj extends App {
+
+  implicit class StringOps(s: String)  {
+    def trimU = s.trim
+  }
+
+  "String".trimU
+
+  new StringOps("ss").trimU
+}
+
+object mm extends App {
+
+  object StringOpsUtil {
+
+    implicit class StringOps(s: String) {
+      def trimU = s.trim
+    }
+
+  }
+  import StringOpsUtil._
+  "String".trimU
+
+}
