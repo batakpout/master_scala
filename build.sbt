bbfd634046b1b228537d219a9fc4fdb7edf61bc8
ssspl <ssspl@ssspls-MacBook-Pro.local>
2021-01-06 00:49:38 +0530

    removing fields from JArray

--- build.sbt
+++ build.sbt
@@ -13,6 +13,7 @@ libraryDependencies ++= Seq(
   "org.scalatest" %% "scalatest" % "3.0.5",
   "org.apache.kafka" %% "kafka" % "2.1.0",
   "com.typesafe.play" %% "play" % "2.8.2",
+  "org.json4s" %% "json4s-jackson" % "3.5.5",
  "org.mockito" % "mockito-core" % "3.3.3" % Test,
   "org.scalamock" %% "scalamock" % "5.0.0" % Test
 
--- src/main/scala/com/aamir/json4s/Program1.scala
+++ src/main/scala/com/aamir/json4s/Program1.scala
@@ -1,8 +1,10 @@
 package com.aamir.json4s
 
 
+import org.json4s.JsonAST.JValue
 import org.json4s.jackson.JsonMethods._
-import org.json4s.{JArray, JObject}
+import org.json4s.{JArray, JField, JObject, JString}
+import play.api.libs.json.{JsObject, JsValue}
 
 import scala.util.Try
 
@@ -19,21 +21,47 @@ object MessageParsers {
 
 object Test extends App {
 
-  val json = s"""{"ack" : "12-09k-9", "fileMetadatas" : [{"key1":"value1"}, {"key2":"value2"}]}"""
+  def removeFields(jsObject: JValue, fields: List[JValue]):JValue = fields match {
+    case Nil => jsObject
+    case h :: t => removeFields(jsObject.remove(_ == h), t)
+  }
+
+  val json = s"""{"ack" : "12-09k-9", "temp1" : "ll", "fileMetadatas" : [{"key1":"value1"}, {"key2":"value2"}, {"key3":"value3"}]}"""
   val jsObject = parse(json).asInstanceOf[JObject]
+
   val ack = jsObject \ "ack"
+  val temp1 = jsObject \ "temp1"
+  //println(jsObject)
+  val res = removeFields(jsObject, List(ack, temp1))
+ // println(res)
+
+
+
 
 
   val fileMetaData = (jsObject \ "fileMetadatas").asInstanceOf[JArray]
-  println(jsObject)
 
-  val newJsObject = jsObject.remove(_ == ack)
-  println(newJsObject)
+  //println(fileMetaData.arr)
+  //val removeKeylist = List("key1", "key3")
+  val jValue = JObject("ack" -> JString("anything"))
 
-  println {
-    ack.values.toString
+  val l = List("key1", "key2")
+
+  def f(jValue: JValue) = {
+    val jObject = jValue.asInstanceOf[JObject]
+    jObject.values.keySet.exists(l.contains(_))
   }
 
-  println(fileMetaData.arr.isEmpty)
+  val jsonArrayKeys =  fileMetaData.arr.filterNot(f)
+
+  println(jsonArrayKeys)
+ //
+
+  //println(jValue.values.keys)
+
+ /* val x = fileMetaData.arr.filterNot(x => ll.contains(x.))
+  println(x)*/
+
+
 
 }
