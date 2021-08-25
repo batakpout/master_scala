2296d0b30aac7e2b41f030d3db731f668606afec
aamirfayaz <dropmeataamir@gmail.com>
2021-08-25 21:13:52 +0530

    kafka examples

---
+++ src/main/scala/devops/code/ConsumerPlaygroundInstance3.scala
@@ -0,0 +1,34 @@
+package devops.code
+
+import org.apache.kafka.clients.consumer.ConsumerConfig._
+import org.apache.kafka.clients.consumer.KafkaConsumer
+import org.apache.kafka.common.serialization.{StringDeserializer, IntegerDeserializer}
+
+import java.time.Duration
+import java.util.Properties
+import scala.jdk.CollectionConverters._
+object ConsumerPlaygroundInstance3 extends App {
+
+  val topicName = "test-1"
+  val consumerProperties = new Properties()
+  consumerProperties.setProperty(BOOTSTRAP_SERVERS_CONFIG, "localhost:9092")
+  consumerProperties.setProperty(GROUP_ID_CONFIG, "group-id-1")
+  consumerProperties.setProperty(AUTO_OFFSET_RESET_CONFIG, "latest") //"earliest" "latest"
+  consumerProperties.setProperty(KEY_DESERIALIZER_CLASS_CONFIG, classOf[IntegerDeserializer].getName)
+  consumerProperties.setProperty(VALUE_DESERIALIZER_CLASS_CONFIG, classOf[StringDeserializer].getName)
+  consumerProperties.setProperty(ENABLE_AUTO_COMMIT_CONFIG, "false")
+
+  val consumer = new KafkaConsumer[Int, String](consumerProperties)
+  consumer.subscribe(List(topicName).asJava)
+
+  println("| key | Message | Partition | Offset |")
+  while (true) {
+    val polledRecord = consumer.poll(Duration.ofSeconds(2)).asScala
+    println(s"Polled ${polledRecord.size} records")
+    for(record <- polledRecord.iterator) {
+      println(s"| ${record.key()} | ${record.value()} | ${record.partition()} | ${record.offset()} |")
+    }
+    Thread.sleep(3000)
+  }
+
+}
---
+++ src/main/scala/devops/code/ProducerPlayground.scala
@@ -0,0 +1,27 @@
+package devops.code
+
+import org.apache.kafka.clients.producer.ProducerConfig._
+import org.apache.kafka.clients.producer.{KafkaProducer, ProducerRecord}
+import org.apache.kafka.common.serialization.{IntegerSerializer, StringSerializer}
+
+import java.util.Properties
+
+object ProducerPlayground extends App {
+
+  val topicName = "test-1"
+  val producerProperties = new Properties()
+  producerProperties.setProperty(BOOTSTRAP_SERVERS_CONFIG, "localhost:9092")
+  producerProperties.setProperty(KEY_SERIALIZER_CLASS_CONFIG, classOf[IntegerSerializer].getName)
+  producerProperties.setProperty(VALUE_SERIALIZER_CLASS_CONFIG, classOf[StringSerializer].getName)
+
+  val producer = new KafkaProducer[Int, String](producerProperties)
+
+  producer.send(new ProducerRecord[Int, String](topicName,0, 10,"Message 1"))
+  producer.send(new ProducerRecord[Int, String](topicName,0, 20,"Message 2"))
+  producer.send(new ProducerRecord[Int, String](topicName,1, 30, "Message 3"))
+  producer.send(new ProducerRecord[Int, String](topicName, 1,40,"Message 4"))
+  producer.send(new ProducerRecord[Int, String](topicName, 2,50,"Message 5"))
+  producer.send(new ProducerRecord[Int, String](topicName,2,60, "Message 6"))
+
+  producer.flush()
+}
